cca619e1f14867912073c796063fdfb989eb055b
lyder7 <lyderaleksander@gmail.com>
2021-12-29 19:51:34 +0100

    ok

---
+++ history.R
@@ -0,0 +1,131 @@
+View(x)
+a <- c("dplyr", "tidyr")
+map(a, tidyverse_packages)
+map(
+  a,
+  print(tidyverse_packages)
+  a
+  dplyr <- pacman::p_functions(package = "dplyr")
+  
+  tidyr <- pacman::p_functions(package = "tidyr")
+  # left_join(dplyr, tidyr, x)
+  # left_join(dyplr, tidyr, x)
+  # merge(dyplr, tidyr, x)
+  
+  cbind(dyplr, tidyr, x) %>%
+    as.data.frame()
+  
+  bdf <- cbind(dyplr, tidyr, x) %>%
+    +as.data.frame()
+  
+  a <- cbind(dyplr, tidyr, x) %>%
+    as.data.frame()
+#  View(a)
+  
+  # unique(a$x)
+  # unique(a$dyplr)
+  # unique(a$2)
+  # unique(a$tidyr)
+  
+  df <- map(a, unique) 
+  str(df)
+  
+  df <- a
+  # a <- <- %>%
+  #   a <- a %>%
+  #   mutate(tidyr = unique(tidyr)) %>%
+  #   print()
+  a <- a %>%
+    +mutate(tidyr = unique(tidyr)) %>%
+    +as.dataframe %> print() %  > %
+  inner_join(tidyr, x)
+  right_join(x, tidyr)
+  library(todyverse)
+  library(tidyselect)
+  library(rlang)
+  pacman::p_functions(package = tidyverse)
+  pacman::p_functions(package = tidyverse) %>%
+    as_vector()
+  a - pacman::p_functions(package = tidyverse) %>%
+    a
+  a <- pacman::p_functions(package = tidyverse) %>%
+    a <- pacman::p_functions(package = tidyverse)
+  library(tidyverse)
+  a <- pacman::p_functions(package = "tidyverse")
+  sessioninfo::package_info()
+  sessioninfo::session_info()
+  a
+  tidyverse_packages()
+  df tidyverse_packages()
+  df <- tidyverse_packages()
+  df
+  class(df)
+  tolower(df)
+  class(df) <- as_vector(df)
+  df
+  print(a)
+  a$1
+  a[1]
+  a[2]
+  df
+  pirny(df)
+  class(df)
+  class(df) = as.data.frame((df))
+  df <- tidyverse_packages()
+  df
+  class(df)
+  class(df) <- as_data_frame(df)
+  list2DF(df)
+  df <- as.data.frame(tidyverse_packages())
+  View(df)
+  library(oacman - )
+  library(pacman)
+  pacman::p_functions(package(c("dplyr", "tidyr")))
+  pacman::p_functions(package = (c("dplyr", "tidyr")))
+  x <- pacman::p_functions(package = (c("dplyr", "tidyr"))) %>%
+    as.data.frame()
+  library(todyverse)
+  library(tidyselect)
+  library(rlang)
+  pacman::p_functions(package = tidyverse)
+  pacman::p_functions(package = tidyverse) %>%
+    as_vector()
+  a - pacman::p_functions(package = tidyverse) %>%
+    a
+  a <- pacman::p_functions(package = tidyverse) %>%
+    a <- pacman::p_functions(package = tidyverse)
+  library(tidyverse)
+  a <- pacman::p_functions(package = "tidyverse")
+  sessioninfo::package_info()
+  sessioninfo::session_info()
+  a
+  tidyverse_packages()
+  df tidyverse_packages()
+  df <- tidyverse_packages()
+  df
+  class(df)
+  tolower(df)
+  class(df) <- as_vector(df)
+  df
+  print(a)
+  a$1
+  a[1]
+  a[2]
+  df
+  pirny(df)
+  class(df)
+  class(df) = as.data.frame((df))
+  df <- tidyverse_packages()
+  df
+  class(df)
+  class(df) <- as_data_frame(df)
+  list2DF(df)
+  df <- as.data.frame(tidyverse_packages())
+  View(df)
+  library(oacman - )
+  library(pacman)
+  pacman::p_functions(package(c("dplyr", "tidyr")))
+  pacman::p_functions(package = (c("dplyr", "tidyr")))
+  x <- pacman::p_functions(package = (c("dplyr", "tidyr"))) %>%
+    as.data.frame()
+  
