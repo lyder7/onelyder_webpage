800309db6575d6a0349cde373002377c8bb08c26
Lyder Aleksander Mathisen <lyderaleksander@gmail.com>
2022-01-19 00:37:59 +0100

    new post things

---
+++ _posts/new-post-r/new-post-r.r
@@ -0,0 +1,26 @@
+
+df_w <- data.frame(d1, 
+                   weekd = 1:length(d1))
+
+
+
+
+mnam <- c("mon", "tue", "wed", "thu", "fri")
+mnam <- c("mon", "tue", "wed", "thu", "fri", "sat", "sun")
+
+df_w$weekd = mnam
+
+
+rbind(df_w, mnam)
+merge(df_w, mnam)
+
+
+df <- data.frame(Date = as.Date("2022-01-10") + 0:4, 
+                 Logical = c(TRUE, FALSE, TRUE, TRUE, FALSE),
+                 Klient = paste("Tema Einendom AS", 0:4), 
+                 Accounting = -2:2, 
+                 hLink = "https://CRAN.R-project.org/",
+                 Percentage = seq(-1, 1, length.out = 5), 
+                 TinyNumber = runif(5)/1e+09, 
+                 stringsAsFactors = FALSE)
+
